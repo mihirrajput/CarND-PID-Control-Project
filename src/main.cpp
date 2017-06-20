1c502776465b48991959513a10719934d6ff6ec5
Mihir Rajput <mihirrajput1123@gmail.com>
2017-06-19 23:35:16 -0400

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(0.01, 0.1, 0.001);
+  pid.Init(0.03, 0.2, 0.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
