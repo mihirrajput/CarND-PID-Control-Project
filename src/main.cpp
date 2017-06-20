f96469c0a0cf3f57fc98143eb528bafeade41a77
Mihir Rajput <mihirrajput1123@gmail.com>
2017-06-20 19:53:44 -0400

    Final tuning completed.
    Car drives in a safe way.

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(0.1, 0.95, 0.0);
+  pid.Init(0.21, 3.5, 0.1);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
